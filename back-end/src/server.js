c1c08fe924ea1eed902edb3e9f10b0f67218d05a
Lê Phương Mỹ Trang <lt25121999@gmail.com>
2025-10-16 10:54:43 +0700

    first commit

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -3,16 +3,28 @@ import tasksRoutes from "./routes/tasksRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 dotenv.config();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 const app = express();
 //middlewares
 app.use(cors());
-app.use(cors({ origin: "http://localhost:5173" }));
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({ origin: "http://localhost:5173" }));
+}
 
 app.use(express.json());
 app.use("/api/tasks", tasksRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  });
+}
+
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Server đang chạy trên cổng ${PORT}`);
